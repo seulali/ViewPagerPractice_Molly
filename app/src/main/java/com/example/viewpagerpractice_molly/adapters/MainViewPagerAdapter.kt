d565f17a1dd66e78535dead592bc4ca32ce8c2e7
seulali <seulali79@gmail.com>
2022-06-06 21:12:48 +0900

    프래그먼트를 위치별로 뿌려주는 어댑터 추가

---
+++ app/src/main/java/com/example/viewpagerpractice_molly/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.viewpagerpractice_molly.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.viewpagerpractice_molly.fragments.GreetingsFragment
+import com.example.viewpagerpractice_molly.fragments.MyInfoFragment
+import com.example.viewpagerpractice_molly.fragments.NameFragment
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+        if(position == 0) {
+            return NameFragment()
+        }
+        else if(position == 1){
+            return MyInfoFragment()
+        }
+        else {
+            return GreetingsFragment()
+        }
+    }
+
+
+}
