43cc0a39375bdfdf5f4de359fde50d21c80811c3
seulali <seulali79@gmail.com>
2022-06-06 21:37:24 +0900

    어댑터 활용하여 페이지별 제목 설정

--- app/src/main/java/com/example/viewpagerpractice_molly/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpagerpractice_molly/adapters/MainViewPagerAdapter.kt
@@ -8,6 +8,17 @@ import com.example.viewpagerpractice_molly.fragments.MyInfoFragment
 import com.example.viewpagerpractice_molly.fragments.NameFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getPageTitle(position: Int): CharSequence? {
+        if(position == 0){
+            return "이름"
+        }
+        else if(position == 1){
+            return "내 정보"
+        }
+        else{
+            return "인사말"
+        }
+    }
     override fun getCount(): Int {
         return 3
     }
